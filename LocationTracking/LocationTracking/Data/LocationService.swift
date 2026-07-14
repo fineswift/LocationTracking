36c0f42cfe3f4e3441435e29116cf4204e2d4db7
fineswift <gjrhkdgh3@naver.com>
2026-07-14 17:16:39 +0900

    feat: LocationService 구현

--- LocationTracking/LocationTracking/Data/LocationService.swift
+++ LocationTracking/LocationTracking/Data/LocationService.swift
@@ -11,18 +11,90 @@ import CoreLocation
 final class LocationService: NSObject, LocationTrackingUseCase {
     private let manager = CLLocationManager()
     
+    private var authContinuation: AsyncStream<LocationAuthorization>.Continuation?
+    private var locationContinuation: AsyncStream<TrackedLocation>.Continuation?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.allowsBackgroundLocationUpdates = true
+        manager.pausesLocationUpdatesAutomatically = false
+    }
+    
     func requestAuthorization() {
-        
+        manager.requestAlwaysAuthorization()
     }
     
     func authorizationUpdates() -> AsyncStream<LocationAuthorization> {
-        AsyncStream { _ in }
+        let (stream, continuation) = AsyncStream.makeStream(of: LocationAuthorization.self)
+        
+        authContinuation = continuation
+        continuation.yield(mapAuthorizationStatus(manager.authorizationStatus))
+        
+        continuation.onTermination = { [weak self] _ in
+            Task { @MainActor [weak self] in
+                self?.authContinuation = nil
+            }
+        }
+        
+        return stream
     }
     
     func start() -> AsyncStream<TrackedLocation> {
-        AsyncStream { _ in }
+        let (stream, continuation) = AsyncStream.makeStream(of: TrackedLocation.self)
+        
+        locationContinuation = continuation
+        manager.startUpdatingLocation()
+        
+        continuation.onTermination = { [weak self] _ in
+            Task { @MainActor [weak self] in
+                self?.locationContinuation = nil
+            }
+        }
+        
+        return stream
     }
 
     func stop() {
+        manager.stopUpdatingLocation()
+        locationContinuation?.finish()
+        locationContinuation = nil
+    }
+    
+    private func mapAuthorizationStatus(_ status: CLAuthorizationStatus) -> LocationAuthorization {
+        switch status {
+        case .notDetermined:
+            return .notDetermined
+        case .restricted:
+            return .restricted
+        case .denied:
+            return .denied
+        case .authorizedAlways:
+            return .authorizedAlways
+        case .authorizedWhenInUse:
+            return .authorizedWhenInUse
+        default:
+            return .notDetermined
+        }
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let domainAuth = mapAuthorizationStatus(manager.authorizationStatus)
+        authContinuation?.yield(domainAuth)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        let tracked = TrackedLocation(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude,
+            timestamp: location.timestamp,
+            activity: "추적중"
+        )
+        locationContinuation?.yield(tracked)
     }
 }
